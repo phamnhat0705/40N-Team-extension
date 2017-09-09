2e48ea1071312e951c3b44b49ad3343b195255da
Tanvn <vungoctan9x@gmail.com>
2017-09-09 17:11:20 +0700

    edit

--- action.js
+++ action.js
@@ -16,48 +16,47 @@ $('#submit').click(function() {
 		var height = $('#height').val() / 100;
 		var bmi = weight / (height * height);
 	    var bmiStatus = "";
+	    var valueBmi = 0;
 
-	    if (bmi < 18.5) { bmiStatus = "Under Weight"; }
+	    if (bmi < 18.5) { bmiStatus = "Under Weight"; valueBmi = 100;}
 	    if (bmi >= 18.5 && bmi < 25) { bmiStatus = "Normal"; }
-	    if (bmi >= 25 && bmi < 30) { bmiStatus = "Over Weight"; }
-	    if (bmi >= 30 && bmi < 40) { bmiStatus = "Obese"; }
-	    if (bmi > 40) { bmiStatus = "Morbidly Obese"; }
+	    if (bmi >= 25 && bmi < 30) { bmiStatus = "Over Weight"; valueBmi = -100;}
+	    if (bmi >= 30 && bmi < 40) { bmiStatus = "Obese"; valueBmi = -200;}
+	    if (bmi > 40) { bmiStatus = "Morbidly Obese"; valueBmi = -300;}
 
 	    var genderValue = 5;
 	    if (gender == "Female") { genderValue = -161; }
 	    var bmr = 10 * weight + 6.25 * height - 5 * age + genderValue;
+	    var calorie = bmr + valueBmi;
 
 	    $('#comment').html(bmiStatus);
 	    $('#result').html("Gender: " + gender + " - Age: " + age + " - Weight: " + weight + " - " + bmr + "<br>");
 
 	    // get json
-	    var listMenuResult = [];
+
 	    $.getJSON("menu.json", function(menu){
+	    	
+		    var listMenuResult = [];
+		    var c = 0;
 	    	for(let data of menu){
-	    		var img = "<img src = ' " + data.image +"' />";
-	    		$('#result').append(img + "<br>");
-	    		listMenuResult.push(data);
+	    		if ((c + parseInt(data.indicator)) < calorie + 50){
+		    		listMenuResult.push(data);
+		   			c += parseInt(data.indicator);
+	    		}
+	    		if (c > calorie - 50) break;
 	    	}
+
+
+		    for (let data of listMenuResult){
+		    	var img = "<img src = ' " + data.image +"' />";
+		    	$('#result').append(img + "<br>");
+		    }
+
 	    });
+
 
 	    $('#div-input').fadeOut();
 	    $('#div-result').fadeIn();
-
-		// ajax handle
-		$.ajax({
-			url: "http://google.com",
-			type: "post",
-			data: {
-				gender: gender,
-				age: age,
-				weight: weight,
-				height: height,
-			},
-
-			success: function(data){
-
-			}
-		});
 	}
 
 	var errorWeight = "";
